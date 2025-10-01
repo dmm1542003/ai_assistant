3bff91c3e69cd4de747e95811625825c6b5e3d2d
Dakota <dmm1542003@yahoo.com>
2025-10-01 00:24:38 -0400

    update

--- python/main.py
+++ python/main.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+def main_function():
+    print("Program started.")
+    while True:
+        # Code to be executed repeatedly
+        user_input = input("Enter 'quite' to exit: ")
+        if user_input.lower()=='quite':
+            break # Exit the main loop
+        print(f"You  entered: {user_input}")
+    print("Program finished.")
+
+if __name__ == "__main__":
+    main_function() 
+
+#This code is for a GUI
+#    import tkinter as tk
+
+#def create_gui():
+#    root = tk.Tk()
+#    root.title("My GUI App")
+
+#    label = tk.Label(root, text="Hello, Tkinter!")
+#    label.pack()
+
+    # This is the main loop for Tkinter
+#    root.mainloop()
+
+#if __name__ == "__main__":
+#    create_gui()
